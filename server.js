1ed5f88a0f7feaa0c1f4b3b98fa483f2c0551855
cuong.duong <cuong.duong@titancorpvn.com>
2018-12-18 14:26:53 +0700

    connect to database

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors')
+var knex = require('knex')
+
+const postgres = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : '',
+    database : 'smart_brain'
+  }
+});
+
+console.log(postgres.select().table('users'))
 
 const app = express()
 
