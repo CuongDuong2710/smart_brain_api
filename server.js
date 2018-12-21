0b41bf70cd4976c31545e39bb1f79579c27c4a6e
cuong.duong <cuong.duong@titancorpvn.com>
2018-12-21 14:51:58 +0700

    create register controller file

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors')
 var knex = require('knex')
 
+const register = require('./controllers/register')
+
 const app = express()
 
 app.use(cors())
@@ -62,35 +64,7 @@ app.post('/signin', (req, res) => {
     .catch(err => res.status(400).json('wrong credentials'))
 })
 
-app.post('/register', (req, res) => {
-  const { email, name, password } = req.body
-
-  const hash = bcrypt.hashSync(password);
-
-  db.transaction(trx => {
-    trx.insert({
-      hash,
-      email
-    })
-    .into('login')
-    .returning('email')
-    .then(loginEmail => {
-      return trx('users')
-        .returning('*')
-        .insert({
-          name,
-          email: loginEmail[0],
-          joined: new Date()
-        })
-        .then(user => {
-          res.json(user[0])
-        })
-    })
-    .then(trx.commit)
-    .catch(trx.rollback)
-  })
-  .catch(err => res.status(400).json('unable register'))
-})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params
