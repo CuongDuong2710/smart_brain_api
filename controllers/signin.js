32a2263a7348f384e6d3177171b54a215a531e57
cuong.duong <cuong.duong@titancorpvn.com>
2018-12-26 16:59:16 +0700

    check empty email/password at sign in

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,5 +1,9 @@
 const handleSignIn =  (db, bcrypt) => (req, res) => {
   const { email, password } = req.body
+
+  if (!email || !password) {
+    return res.status(400).json('incorrect form submission')
+  }
   
   db.select('email', 'hash')
     .from('login')
